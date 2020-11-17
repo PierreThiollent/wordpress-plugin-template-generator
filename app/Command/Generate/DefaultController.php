794b7c3bd75bbbfa0d4c6f19007db4c016c6db8d
Pierre Thiollent <pierre.thiollent76@gmail.com>
2020-11-17 20:34:12 +0100

    📦 FEAT: Add logic to ask user for plugin slug and copy template plugin folder  content into new folder with plugin slug name

--- app/Command/Generate/DefaultController.php
+++ app/Command/Generate/DefaultController.php
@@ -4,21 +4,45 @@ namespace App\Command\Generate;
 
 use Minicli\Command\CommandController;
 use Minicli\Input;
+use App\Helpers\FileHelper;
 
 class DefaultController extends CommandController
 {
     public function handle()
     {
-        $input = new Input();
-
         $this->getPrinter()->info('INFO');
         $this->getPrinter()->info('This command will let you generate a template plugin folder.');
         $this->getPrinter()->info('It will ask you for the plugin name, slug, URI, the author name, URI and email.');
         $this->getPrinter()->newline();
 
-        $input->setPrompt('Plugin name : ');
+        $input = new Input();
+        $file_helper = new FileHelper();
 
+        $input->setPrompt('Plugin slug : ');
+        $plugin_slug = $input->read();
+
+        // If plugin slug is blank
+        if (!$plugin_slug) {
+            while (!$plugin_slug) {
+                $this->getPrinter()->error('The plugin slug can\'t be empty');
+                $input->setPrompt('Plugin slug : ');
+                $this->getPrinter()->newline();
+                $plugin_slug = $input->read();
+            }
+        }
+
+        $plugin_folder = './source/' . $plugin_slug;
+        // Copy template plugin into new folder with plugin slug name
+        $file_helper->recursive_copy('./source/plugin-name', $plugin_folder);
+
+        $fileList = $file_helper->getDirectoryContent($plugin_folder);
+
+        // Rename all plugin files
+        foreach ($fileList as $file) {
+            rename($file, str_replace('plugin-name', $plugin_slug, $file));
+        }
+
+        $input->setPrompt('Plugin Name : ');
         $plugin_name = $input->read();
-        echo $plugin_name;
     }
 }
