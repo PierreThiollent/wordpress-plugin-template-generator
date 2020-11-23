aa4210116474103945840b6bddf3d30f463dbced
Pierre Thiollent <pierre.thiollent76@gmail.com>
2020-11-23 19:50:14 +0100

    📦 FEAT: Add logic to ask plugin URI, check URI is correct and replace it into all files. Same logic for plugin name, author name, author email and author URI. Need some refactor to improve readability. Need more controls of prompt too.

--- app/Command/Generate/DefaultController.php
+++ app/Command/Generate/DefaultController.php
@@ -23,6 +23,7 @@ class DefaultController extends CommandController
         $input = new Input();
         $file_helper = new FileHelper();
 
+        // Ask plugin slug
         $input->setPrompt('Plugin slug : ');
         $plugin_slug = $input->read();
 
@@ -45,27 +46,134 @@ class DefaultController extends CommandController
             }
         }
 
-        // TODO:
         $plugin_destination_folder = './source/' . $plugin_slug;
 
         // Copy template plugin into new folder with plugin slug name
         $file_helper->recursive_copy('./source/plugin-name', $plugin_destination_folder);
 
+        // Get file names (directories and subdirectories)
         $file_list = $file_helper->get_directory_content($plugin_destination_folder);
 
-        // Rename all plugin files
+        // Rename all plugin files recursively
         $file_helper->rename_files($file_list, $plugin_slug);
 
-        // Replace plugin-name
-        $file_helper->rename_files_content('/plugin-name/', $plugin_slug, $plugin_destination_folder);
-
+        // Ask plugin name
         $input->setPrompt('Plugin Name : ');
         $plugin_name = $input->read();
 
-        // Remove whitespaces and add CamelCase for Classes
-        $plugin_name = str_replace(' ', '_', ucwords($plugin_name, ' '));
+        // If plugin name is blank
+        if (!$plugin_name) {
+            while (!$plugin_name) {
+                $this->getPrinter()->error('The plugin name can\'t be empty');
+                $input->setPrompt('Plugin name : ');
+                $this->getPrinter()->newline();
+                $plugin_name = $input->read();
+            }
+        }
 
-        // Replace Plugin_Name
-        $file_helper->rename_files_content('/Plugin_Name/', $plugin_name, $plugin_destination_folder);
+        // Replace plugin name (WordPress Plugin Boilerplate)
+        $file_helper->rename_files_content('/WordPress Plugin Boilerplate/', $plugin_name, $plugin_destination_folder);
+
+        $input->setPrompt('Plugin URI : ');
+        $plugin_uri = $input->read();
+
+        // If plugin URI is blank
+        if (!$plugin_uri) {
+            while (!$plugin_uri) {
+                $this->getPrinter()->error('The plugin URI can\'t be empty');
+                $input->setPrompt('Plugin URI : ');
+                $this->getPrinter()->newline();
+                $plugin_uri = $input->read();
+            }
+        } elseif (!filter_var($plugin_uri, FILTER_VALIDATE_URL)) {
+            while (!filter_var($plugin_uri, FILTER_VALIDATE_URL)) {
+                $this->getPrinter()->error('The plugin URI is not valid.');
+                $input->setPrompt('Plugin URI : ');
+                $this->getPrinter()->newline();
+                $plugin_uri = $input->read();
+            }
+        }
+
+        // Replace plugin URI
+        $file_helper->rename_files_content('/http:\/\/example.com/', $plugin_uri, $plugin_destination_folder);
+        $file_helper->rename_files_content('/http:\/\/example.com\/plugin-name-uri\//', $plugin_uri, $plugin_destination_folder);
+
+        // Replace plugin-name string into all files (comments, filenames of require, etc)
+        $file_helper->rename_files_content('/plugin-name/', $plugin_slug, $plugin_destination_folder);
+
+        // Replace classes name (Plugin_Name)
+        $file_helper->rename_files_content('/Plugin_Name/', str_replace(' ', '_', ucwords($plugin_name, ' ')), $plugin_destination_folder);
+        // Replace const PLUGIN_NAME_VERSION
+        $file_helper->rename_files_content('/PLUGIN_NAME/', str_replace(' ', '_', strtoupper($plugin_name)), $plugin_destination_folder);
+        // Replace function names
+        $file_helper->rename_files_content('/plugin_name/', str_replace(' ', '_', strtolower($plugin_name)), $plugin_destination_folder);
+
+        // Ask author name
+        $input->setPrompt('👨🏻‍💻 Author name : ');
+        $author_name = $input->read();
+
+        // If author name is not empty
+        if (!$author_name) {
+            while (!$author_name) {
+                $this->getPrinter()->error('The author name can\'t be empty.');
+                $input->setPrompt('Author name : ');
+                $this->getPrinter()->newline();
+                $author_name = $input->read();
+            }
+        }
+
+        // Replace author name (Your Name or Your Company)
+        $file_helper->rename_files_content('/Your Name or Your Company/', $author_name, $plugin_destination_folder);
+
+        // Ask author email
+        $input->setPrompt('👨🏻‍💻 Author email : ');
+        $author_email = $input->read();
+
+        // If author email is blank
+        if (!$author_email) {
+            while (!$author_email) {
+                $this->getPrinter()->error('The author email can\'t be empty.');
+                $input->setPrompt('Author email : ');
+                $this->getPrinter()->newline();
+                $author_email = $input->read();
+            }
+            // If author email is not a valid email
+        } elseif (!filter_var($author_email, FILTER_VALIDATE_EMAIL)) {
+            while (!filter_var($author_email, FILTER_VALIDATE_EMAIL)) {
+                $this->getPrinter()->error('The author email is not valid.');
+                $input->setPrompt('Author email : ');
+                $this->getPrinter()->newline();
+                $author_email = $input->read();
+            }
+        }
+        // Replace author name and email (Your Name <email@example.com>)
+        $file_helper->rename_files_content('/Your Name <email@example.com>/', $author_name . ' <' . $author_email . '>', $plugin_destination_folder);
+
+        // Ask author URI
+        $input->setPrompt('👨🏻‍💻 Author URI : ');
+        $author_uri = $input->read();
+
+        // If author email is blank
+        if (!$author_uri) {
+            while (!$author_uri) {
+                $this->getPrinter()->error('👨🏻‍💻 The author URI can\'t be empty.');
+                $input->setPrompt('👨🏻‍💻 Author URI : ');
+                $this->getPrinter()->newline();
+                $author_uri = $input->read();
+            }
+            // If author URI is not a valid email
+        } elseif (!filter_var($author_uri, FILTER_VALIDATE_URL)) {
+            while (!filter_var($author_uri, FILTER_VALIDATE_URL)) {
+                $this->getPrinter()->error('👨🏻‍💻 The author URI is not valid.');
+                $input->setPrompt('👨🏻‍💻 Author URI : ');
+                $this->getPrinter()->newline();
+                $author_uri = $input->read();
+            }
+        }
+
+        // Replace author URI
+        $file_helper->rename_files_content('/http:\/\/example.com\//', $plugin_uri, $plugin_destination_folder);
     }
+
+    // TODO Ask for plugin description
 }
